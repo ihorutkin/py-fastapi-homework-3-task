9f1abadb590a2593ba77e5c2d67d3c98b3a5a3a8
Ihor Utkin <igorutkin2002@gmail.com>
2026-06-21 14:45:16 +0200

    Implement User Registration Endpoint is Done: all tests have passed

--- src/routes/accounts.py
+++ src/routes/accounts.py
@@ -3,7 +3,7 @@ from typing import cast
 
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy import select, delete
-from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session, joinedload
 
@@ -15,11 +15,68 @@ from database import (
     UserGroupEnum,
     ActivationTokenModel,
     PasswordResetTokenModel,
-    RefreshTokenModel
+    RefreshTokenModel,
 )
 from exceptions import BaseSecurityError
+from schemas import UserRegistrationResponseSchema, UserRegistrationRequestSchema
 from security.interfaces import JWTAuthManagerInterface
+from security.passwords import hash_password
 
 router = APIRouter()
 
 # Write your code here
+
+
+@router.post(
+    "/register/",
+    response_model=UserRegistrationResponseSchema,
+    status_code=status.HTTP_201_CREATED,
+)
+async def register_new_user(
+    user_data: UserRegistrationRequestSchema, session: AsyncSession = Depends(get_db)
+):
+    db_user = await session.execute(
+        select(UserModel).where(UserModel.email == user_data.email)
+    )
+    existing_user = db_user.scalar_one_or_none()
+
+    if existing_user is not None:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"A user with this email {user_data.email} already exists.",
+        )
+
+    group_user_result = await session.execute(
+        select(UserGroupModel).where(UserGroupModel.name == UserGroupEnum.USER)
+    )
+    group_user = group_user_result.scalar_one_or_none()
+
+    if group_user is None:
+        group_user = UserGroupModel(name=UserGroupEnum.USER)
+        session.add(group_user)
+
+        await session.flush()
+
+    try:
+        new_user = UserModel.create(
+            email=user_data.email,
+            raw_password=user_data.password,
+            group_id=group_user.id,
+        )
+
+        session.add(new_user)
+        await session.flush()
+
+        activation_token = ActivationTokenModel(user_id=new_user.id)
+        session.add(activation_token)
+
+        await session.commit()
+        await session.refresh(new_user)
+    except SQLAlchemyError:
+        await session.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occurred during user creation.",
+        )
+
+    return new_user
--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -1,6 +1,54 @@
-from pydantic import BaseModel, EmailStr, field_validator
+from pydantic import BaseModel, EmailStr, field_validator, ConfigDict
 
-from database import accounts_validators
-
+from database import accounts_validators, UserGroupEnum
 
 # Write your code here
+
+
+class UserRegistrationRequestSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    email: EmailStr
+    password: str
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value: str) -> str:
+        return accounts_validators.validate_password_strength(value)
+
+
+class UserRegistrationResponseSchema(BaseModel):
+    id: int
+    email: EmailStr
+
+
+class UserActivationRequestSchema(BaseModel):
+    pass
+
+
+class MessageResponseSchema(BaseModel):
+    pass
+
+
+class PasswordResetRequestSchema(BaseModel):
+    pass
+
+
+class PasswordResetCompleteRequestSchema(BaseModel):
+    pass
+
+
+class UserLoginResponseSchema(BaseModel):
+    pass
+
+
+class UserLoginRequestSchema(BaseModel):
+    pass
+
+
+class TokenRefreshRequestSchema(BaseModel):
+    pass
+
+
+class TokenRefreshResponseSchema(BaseModel):
+    pass
